d31b984527749b1dc2cf5c9cec54df502cba9d16
Dias <dias97_12@gmail.ru>
2016-04-12 16:27:02 +0600

    changed something

--- src/javaprojectsample/JavaProjectSample.java
+++ src/javaprojectsample/JavaProjectSample.java
@@ -33,6 +33,7 @@ public class JavaProjectSample extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                System.out.println("HELLO!");
             }
         });
         
