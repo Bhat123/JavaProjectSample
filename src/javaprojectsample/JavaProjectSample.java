01dcc23afdcb1548cfe50ad1532b929064d493e1
Dias <dias97_12@gmail.ru>
2016-04-14 21:34:37 +0600

    one line added dias

--- src/javaprojectsample/JavaProjectSample.java
+++ src/javaprojectsample/JavaProjectSample.java
@@ -47,6 +47,7 @@ public class JavaProjectSample extends Application {
     public void start(Stage primaryStage) throws Exception {
     AnchorPane ancPane = new AnchorPane();  
         System.out.println("kjsabdkasbdkbh");
+        System.out.println("askjdnkjasdnkj");
         
      Rectangle rect = new Rectangle (100, 40, 100, 100);
      rect.setArcHeight(50);
