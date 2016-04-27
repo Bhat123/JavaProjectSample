e6b5e0e83f720d8ceb786e3be9f52d9d4360b98b
sarah <sarah.hiydandao@gmail.com>
2016-04-28 00:36:38 +0600

    checking precedure

--- src/javaprojectsample/Enemy.java
+++ src/javaprojectsample/Enemy.java
@@ -12,12 +12,28 @@ import java.util.logging.Logger;
  *
  * @author hiyandao101
  */
-public class Enemy extends Thread{
+public class Enemy extends Thread {
+
     @Override
-    public void run(){
+    public void run() {
         Tank enemy = new Tank(700, 500);
-        while(true){
-            
+        while (true) {
+            if (enemy.y > JavaProjectSample.tank.getY()) {
+                enemy.rotate(2);
+                while (enemy.y + enemy.eTank.getTranslateY() - JavaProjectSample.tank.getY() - JavaProjectSample.tank.getTranslateY() > 20) {
+                    enemy.move(1, 10);
+                    System.out.println("enemy " + enemy.eTank.getTranslateY());
+                    System.out.println("mine " + JavaProjectSample.tank.getY());
+                }
+                System.out.println("worked");
+                enemy.rotate(3);
+                enemy.fire();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Enemy.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
